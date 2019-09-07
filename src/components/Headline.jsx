bbbfd99835acaf2fb5b04154f83c5e468d4645fc
Jan Sedlon <hanisek.sedlon@gmail.com>
2019-09-07 17:02:20 +0200

    / Renamed files and fixed ESLint errors

--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -5,15 +5,21 @@ import classes from './Headline.css'
 
 const Headline = ({
   name,
+  changeName,
 }) => (
   <div>
-    <h1 className={classes.headline}>Hello {name}! :)</h1>
-    <button onClick={changeName}>Click me!</button>
+    <h1 className={classes.headline}>
+      Hello
+      {name}
+      ! :)
+    </h1>
+    <button onClick={changeName} type="button">Click me!</button>
   </div>
 )
 
 Headline.propTypes = {
   name: PropTypes.string.isRequired,
+  changeName: PropTypes.func.isRequired,
 }
 
 export default Headline
